c48f4764f294608f6f4d6b60524bef5b2b22d114
invtr07 <95737990+invtr07@users.noreply.github.com>
2024-04-04 15:40:22 +0100

    added homepage with listview that is binded to data model class

--- coursework/coursework/MainPage.xaml.cs
+++ coursework/coursework/MainPage.xaml.cs
@@ -10,9 +10,26 @@ namespace coursework
 {
     public partial class MainPage : ContentPage
     {
+        public MenuItems[] menuItems;
+
         public MainPage()
         {
             InitializeComponent();
+
+            menuItems = new MenuItems[]
+            {
+                new MenuItems{ Title="Hierarchy", Descr="Browse and evaluate" },
+                new MenuItems{ Title="Decision alternatives", Descr="View rating scores" }
+            };
+
+            ListView.ItemsSource = menuItems;
+        }
+
+        public class MenuItems
+        {
+            public string Title { get; set; }
+            public string Descr { get; set; }
+
         }
     }
 }
